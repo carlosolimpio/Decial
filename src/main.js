bf837d5e38122490a5f677a694ebe929ad93137b
Carlos Olimpio <carlossolimpio@hotmail.com>
2018-07-09 21:58:21 -0300

    troca de template

--- src/main.js
+++ src/main.js
@@ -1,25 +1,32 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
 import App from './App.vue'
+import store from './store'
+import VueRouter from 'vue-router';
+import { routes } from './router/routes';
+import { index } from './components/index';
 
-// LightBootstrap plugin
-import LightBootstrap from './light-bootstrap-main'
 
-// router setup
-import routes from './routes/routes'
-// plugin setup
-Vue.use(VueRouter)
-Vue.use(LightBootstrap)
+// Form Wizard
+import VueFormWizard from 'vue-form-wizard'
+Vue.use(VueFormWizard)
 
-// configure router
+// Table
+import Vuetable from 'vuetable-2'
+Vue.use(Vuetable)
+
+// Router
+Vue.use(VueRouter);
 const router = new VueRouter({
-  routes, // short for routes: routes
-  linkActiveClass: 'nav-item active'
-})
+    routes,
+    linkActiveClass: 'open active',
+    scrollBehavior: () => ({ y: 0 }),
+    mode: 'hash'
+});
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
+  store,
   render: h => h(App),
-  router
+  components: { App }
 })
