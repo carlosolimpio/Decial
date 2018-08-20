5ab4f65330686c5f9b004d8daaa5796680287704
Ramices <ramicesmoises@gmail.com>
2018-08-19 21:44:38 -0300

    NAO É POSSIVEL

--- Servidor/DecialServidor/app/controllers/PontoRiscoController.java
+++ Servidor/DecialServidor/app/controllers/PontoRiscoController.java
@@ -1,5 +1,23 @@
 package controllers;
 
-public class PontoRiscoController {
+import java.util.List;
 
+import interceptors.ApiInterceptor;
+import models.PontoRisco;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+import services.PontoRiscoService;
+
+@play.mvc.With({ ApiInterceptor.class })
+public class PontoRiscoController extends Controller {
+	
+	
+	public Result getAll()
+	{
+		List<PontoRisco> lpr = PontoRiscoService.getAll();
+		
+		return ok(Json.toJson(lpr));
+	}
+	
 }
--- Servidor/DecialServidor/app/services/PontoRiscoService.java
+++ Servidor/DecialServidor/app/services/PontoRiscoService.java
@@ -1,5 +1,16 @@
 package services;
 
+import java.util.List;
+
+import com.avaje.ebean.Ebean;
+
+import models.PontoRisco;
+
 public class PontoRiscoService {
 
+	
+	public static List<PontoRisco> getAll()
+	{
+		return Ebean.createQuery(PontoRisco.class).where().findList();
+	}
 }
